e1714776b3db2abe2ec29531858b4e1e867ff414
Brian Szucs <brian@thedietdoc.com>
2017-02-02 14:27:52 -0600

    initial commit

---
+++ the-macro-doc.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: The Macro Doc
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A full featured macro-nutrient calcualtor provided by Dr. Joe Klemczewski
+Version: 1.0
+Author: The Zooxinator
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
